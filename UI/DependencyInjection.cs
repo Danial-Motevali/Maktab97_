73d44827bbd51370167cbb2f59b1140f938955fa
Danial Motevali <danial.motevali.82@gmail.com>
2023-10-30 20:16:46 +0330

    configure the services in program

---
+++ UI/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using App.Domain.Core.Contract.Repository;
+using App.Infrastructure.DataAccess.Repository;
+using AutoMapper;
+using System.Runtime.CompilerServices;
+
+namespace UI
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddService(this IServiceCollection services)
+        {
+            //configure repository
+
+            services.AddScoped<IAddressRepository, AddressRepository>();
+            services.AddScoped<IAuctionRepository, AuctionRepository>();
+            services.AddScoped<ICartRepository, CartRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ICommentRepository, CommentRepository>();
+            services.AddScoped<IPictourRepository, PictureRepository>();
+            services.AddScoped<IPriceRepository, PriceRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IUSerRepository, UserRepository>();
+            services.AddScoped<IShopRepository, ShopRepository>();
+            services.AddScoped<IWageRepository, WageRepository>();
+
+            return services;
+        }
+    }
+}
--- UI/Program.cs
+++ UI/Program.cs
@@ -1,4 +1,6 @@
+using App.Domain.Core.Contract.Repository;
 using App.Infrastructure.Data.EF;
+using App.Infrastructure.DataAccess.Repository;
 using Microsoft.EntityFrameworkCore;
 
 namespace UI
@@ -11,10 +13,25 @@ namespace UI
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
             builder.Services.AddDbContext<ApplicationDbContext>(option =>
                      option.UseSqlServer(builder.Configuration.GetConnectionString("DefultConnection"))
             );
+
             builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
+            //configure my services
+            builder.Services.AddScoped<IAddressRepository, AddressRepository>();
+            builder.Services.AddScoped<IAuctionRepository, AuctionRepository>();
+            builder.Services.AddScoped<ICartRepository, CartRepository>();
+            builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+            builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+            builder.Services.AddScoped<IPictourRepository, PictureRepository>();
+            builder.Services.AddScoped<IPriceRepository, PriceRepository>();
+            builder.Services.AddScoped<IProductRepository, ProductRepository>();
+            builder.Services.AddScoped<IUSerRepository, UserRepository>();
+            builder.Services.AddScoped<IShopRepository, ShopRepository>();
+            builder.Services.AddScoped<IWageRepository, WageRepository>();
 
             var app = builder.Build();
 
