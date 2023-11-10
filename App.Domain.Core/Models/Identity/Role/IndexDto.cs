21f66afc88647f7d1ecd832db4f17cbdddfd6e7b
Danial Motevali <danial.motevali.82@gmail.com>
2023-11-10 13:53:57 +0330

    add FirstName and LastName to dto

--- App.Domain.Core/Models/Identity/Role/IndexDto.cs
+++ App.Domain.Core/Models/Identity/Role/IndexDto.cs
@@ -9,6 +9,8 @@ namespace App.Domain.Core.Models.Identity.Role
     public class IndexDto
     {
         public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
     }
