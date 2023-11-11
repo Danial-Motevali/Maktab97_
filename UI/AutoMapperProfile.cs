c4cc87533f930f0fe1314ea2216796af9efea4f8
Danial Motevali <danial.motevali.82@gmail.com>
2023-11-11 12:31:53 +0330

    bring auto mapper profile in ui and do the mapper opration in contorller

---
+++ UI/AutoMapperProfile.cs
@@ -0,0 +1,47 @@
+﻿using App.Domain.Core.Entities;
+using App.Domain.Core.Models.Dto;
+using App.Domain.Core.Models.Identity.Entites;
+using AutoMapper;
+
+namespace UI
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            //for add part
+            CreateMap<AddressDtoInput, Address>().ReverseMap();
+            CreateMap<AdminMyDtoInput, MyAdmin>().ReverseMap();
+            CreateMap<AuctionDtoInput, Auction>().ReverseMap();
+            CreateMap<BuyerDtoInput, Buyer>().ReverseMap();
+            CreateMap<CartDtoInput, Cart>().ReverseMap();
+            CreateMap<CategoryDtoInput, Category>().ReverseMap();
+            CreateMap<CommentDtoInput, Comment>().ReverseMap();
+            CreateMap<InventoryDtoInput, Inventory>().ReverseMap();
+            CreateMap<MedalDtoInput, Medal>().ReverseMap();
+            CreateMap<PictureDtoInput, Picture>().ReverseMap();
+            CreateMap<PriceDtoInput, Price>().ReverseMap();
+            CreateMap<ProductDtoInput, Product>().ReverseMap();
+            CreateMap<SellerDtoInput, Seller>().ReverseMap();
+            CreateMap<ShopDtoInput, Shop>().ReverseMap();
+            CreateMap<WageDtoInput, Wage>().ReverseMap();
+
+            //for get part
+            CreateMap<Address, AddressDtoOutput>().ReverseMap();
+            CreateMap<MyAdmin, AdminMyDtoOutput>().ReverseMap();
+            CreateMap<Auction, AuctionDtoOutput>().ReverseMap();
+            CreateMap<Buyer, BuyerDtoOutput>().ReverseMap();
+            CreateMap<Cart, CartDtoOutput>().ReverseMap();
+            CreateMap<Category, CategoryDtoOutput>().ReverseMap();
+            CreateMap<Comment, CommentDtoOutput>().ReverseMap();
+            CreateMap<Inventory, InventoryDtoOutput>().ReverseMap();
+            CreateMap<Medal, MedalDtoOutput>().ReverseMap();
+            CreateMap<Picture, PictureDtoOutput>().ReverseMap();
+            CreateMap<Price, PriceDtoOutput>().ReverseMap();
+            CreateMap<Product, ProductDtoOutput>().ReverseMap();
+            CreateMap<Seller, SellerDtoOutput>().ReverseMap();
+            CreateMap<Shop, ShopDtoOutput>().ReverseMap();
+            CreateMap<Wage, WageDtoOutput>().ReverseMap();
+        }
+    }
+}
--- UI/Controllers/AdminController.cs
+++ UI/Controllers/AdminController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using App.Domain.Core.Contract.Repository;
+using App.Domain.Core.Contract.Services;
+using App.Domain.Core.Models.Dto;
+using App.Domain.Services.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,10 +11,32 @@ namespace UI.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly IShopService _shopService;
+        private readonly IMapper _mapper;
+        public AdminController(IShopService shopServices, IMapper mapper)
+        {
+            _shopService = shopServices;
+            _mapper = mapper;
+        }
+
         // GET: AdminController
         public ActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> ShowTheShop(CancellationToken cancellation)
+        {
+            var shops = await _shopService.GetAll(cancellation);
+
+            List<ShopDtoOutput> result = new List<ShopDtoOutput>();
+
+            foreach (var shopDto in shops)
+            {
+                result.Add(_mapper.Map<ShopDtoOutput>(shopDto));
+            }
+
+            return View(shops);
         }
 
         // GET: AdminController/Details/5
