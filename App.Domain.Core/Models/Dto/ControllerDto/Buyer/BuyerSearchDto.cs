2b7927f252fdee27602c13b0cc2bf2e10362b9ba
Danial Motevali <danial.motevali.82@gmail.com>
2023-11-24 19:42:19 +0330

    add search with user name service

---
+++ App.Domain.Core/Models/Dto/ControllerDto/Buyer/BuyerSearchDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Core.Models.Dto.ControllerDto.Buyer
+{
+    public class BuyerSearchDto
+    {
+        public string? ProductTitle { get; set; }
+
+        public string? ProductUrl { get; set; }
+
+        public string? Category { get; set; }
+
+        public string? SellerName { get; set; }
+
+        public int ProductPrice { get; set; }
+
+        public int SellerId { get; set; }
+
+        public int ShopId { get; set; }
+
+        public int ProductId { get; set; }
+    }
+}
