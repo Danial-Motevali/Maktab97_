11064cb6b8ff4e21e2fd35c5eacf062a1b0f12c7
Danial Motevali <danial.motevali.82@gmail.com>
2023-11-26 21:08:08 +0330

    geting value from appsetting

---
+++ App.Domain.Services/AppSetting.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Services
+{
+    public class AppSetting
+    {
+        public AppSetting(IConfiguration configuration) 
+        {
+            GoldWage = configuration.GetValue<int>("GoldWage");
+
+            SilverWage = configuration.GetValue<int>("SilverWage");
+
+            CopperWage = configuration.GetValue<int>("CopperWage");
+        }
+
+        public int GoldWage { get; private set; }
+
+        public int SilverWage { get; private set; }
+
+        public int CopperWage { get; private set; }
+    }
+}
