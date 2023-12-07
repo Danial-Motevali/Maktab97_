9ea16b8c349e8ae1358a46b5c6597614a9dfe390
Danial Motevali <danial.motevali.82@gmail.com>
2023-12-07 16:51:41 +0330

    .

---
+++ App.Infrastructure.Data.EF/Migrations/20231207131458_v19.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Infrastructure.Data.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class v19 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Auctions",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Auctions_UserId",
+                table: "Auctions",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Auctions_AspNetUsers_UserId",
+                table: "Auctions",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Auctions_AspNetUsers_UserId",
+                table: "Auctions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Auctions_UserId",
+                table: "Auctions");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Auctions");
+        }
+    }
+}
